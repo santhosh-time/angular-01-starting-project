81a60857268a0b9f0299927eff17aa83728c7042
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2025-05-28 23:58:27 +0530

    added 'users componen' and outputting dynamic content with string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 // This is the main application component for an Angular application.
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
